fc9500951888c379e64e7addae02f1e81ba6ba24
Shushitrashhhh <sona.gogeta@gmail.com>
2026-09-13 19:27:06 +0530

    fix:link updated

--- app/page.tsx
+++ app/page.tsx
@@ -170,7 +170,7 @@ export default function Home() {
             </a>
             <a
               className="cta"
-              href="https://drive.google.com/file/d/1K0F3kKYIcjdr901Ekwt9rZx11q8k2cbO/view?usp=sharing"
+              href="https://drive.google.com/file/d/1K0F3kKYIcjdr901Ekwt9rZx11q8k2cbO/view"
               target="_blank"
               rel="noreferrer"
             >
